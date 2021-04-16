974ef12b844cca3ba24316c0b3f95c8ad7619a0d
pankov <frostkslo1@yandex.ru>
2021-04-16 18:45:54 +0300

    Марат лох

---
+++ Marat_Lox/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Marat_Lox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            if (a > b)
+            {
+                if (a > c)
+
+                    Console.WriteLine("" + a + " " + c + " " + b);
+
+                else
+
+                    Console.WriteLine("" + c + " " + a + " " + b);
+
+            }else if (b > c)
+            {
+                if (c > a)
+
+                    Console.WriteLine("" + b + " " + c + " " + a);
+
+                else
+
+                    Console.WriteLine("" + b + " " + a + " " + c);
+                
+            }else
+                Console.WriteLine("" + c + " " + b + " " + a);
+            
+
+        }
+    }
+}
