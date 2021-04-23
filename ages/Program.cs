42f35eecba475a6203aff76cd3ec2ca587a5a7f5
pankov <frostkslo1@yandex.ru>
2021-04-23 21:27:26 +0300

    Final

--- ages/Program.cs
+++ ages/Program.cs
@@ -4,27 +4,20 @@ namespace ages
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             string s = Console.ReadLine();
-            byte firstNumber = 0;
-            byte secondNumber;
-            if (s.Length == 1)
-                secondNumber = Convert.ToByte(s[0].ToString());
-            else
-            {
-                firstNumber = Convert.ToByte(s[0].ToString());
-                secondNumber = Convert.ToByte(s[1].ToString());
-            }
+            byte firstNumber = (byte) (Convert.ToInt16(s) / 10);
+            byte secondNumber = Convert.ToByte(s[^1].ToString());
             switch (secondNumber)
             {
                 case 1:
-                    Console.WriteLine(firstNumber != 1 ? "Вам {0} год." : "Вам {0} лет.", s);
+                    Console.WriteLine(firstNumber.ToString()[^1] != '1' ? "Вам {0} год." : "Вам {0} лет.", s);
                     break;
                 case 2:
                 case 3:
                 case 4:
-                    Console.WriteLine(firstNumber != 1 ? "Вам {0} года." : "Вам {0} лет.", s);
+                    Console.WriteLine(firstNumber.ToString()[^1] != '1' ? "Вам {0} года." : "Вам {0} лет.", s);
                     break;
                 default:
                     Console.WriteLine("Вам {0} лет.", s);
