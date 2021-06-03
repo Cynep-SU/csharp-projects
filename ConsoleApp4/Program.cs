d54686f41e8db9748b38d729dfb530e3935820f4
pankov <frostkslo1@yandex.ru>
2021-06-03 20:30:34 +0300

    Кто спишет, тот лох. Люблю вас.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int sum = 0;
+            Console.WriteLine("Write X number");
+            int x = Int32.Parse(Console.ReadLine()!);
+            do
+            {
+                n = Int32.Parse(Console.ReadLine()!);
+                if (n % x == 0)
+                    sum += n;
+            } while (n > 0);
+            Console.WriteLine($"Sum of multiples of x = {sum}");
+        }
+    }
+}
