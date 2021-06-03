5e627485b06b59283b12bbd6b8cdaa7467eee34b
pankov <frostkslo1@yandex.ru>
2021-06-03 21:27:26 +0300

    это какой-то пример

---
+++ это-какой-пример/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace это_какой_пример
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 100; i += 2)
+                Console.Write($"{i} ");
+        }
+    }
+}
